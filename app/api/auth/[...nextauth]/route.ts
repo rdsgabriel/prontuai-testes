07fca9c9b35efd8a3c842e05e94be61d9767d425
Gabriel Rodrigues <rdsgabriel.dev@gmail.com>
2025-07-15 14:17:18 -0300

    fix(auth): remove callbackurl

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -11,7 +11,7 @@ const authOptions = {
   ],
   pages: {
     signIn: "/login",
-    callbackUrl: "/chat",
+    //callbackUrl: "/chat",
   },
   callbacks: {
     async signIn({ profile }: { profile?: Profile }) {
